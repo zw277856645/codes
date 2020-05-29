f46f2f7ef2d8f59232cf0893aee616358d146bd5
277856645@qq.com <zw15251778128>
2020-05-29 08:49:08 +0800

    add

---
+++ other.js
@@ -0,0 +1,20 @@
+// --------------------------------------------------------------------------------------------
+// 获取路径值
+
+let obj = { a: { b: { c: 1 } } };
+
+function get(obj, path) {
+    let props = path.split('.');
+
+    if (typeof obj !== 'object' || obj == null) {
+        return obj
+    } else if (props.length === 1) {
+        return obj[ props[ 0 ] ];
+    } else {
+        return get(obj[ props[ 0 ] ], props.slice(1).join('.'));
+    }
+}
+
+console.log(get(obj, 'a'));
+console.log(get(obj, 'a.b'));
+console.log(get(obj, 'a.b.c'));
