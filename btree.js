74def4d7f33e71d9a8f3869f4e9840b40d9d3d72
teame_11 <zw177899@alibaba-inc.com>
2020-06-12 18:18:31 +0800

    add

--- btree.js
+++ btree.js
@@ -1,35 +1,45 @@
-var test = function (s) {
-    if (s.length < 2) {
-        return s;
+var test = function (A) {
+    let res = [];
+
+    function flip(end) {
+        if (end === 1) {
+            return;
+        }
+
+        let index = 0;
+        let max = A[ 0 ];
+
+        for (let i = 1; i <= end; i++) {
+            if (A[ i ] > max) {
+                max = A[ i ];
+                index = i;
+            }
+        }
+
+        reverse(0, index);
+        res.push(index + 1);
+
+        reverse(0, end);
+        res.push(end + 1);
+
+        flip(end - 1);
     }
 
-    let dp = new Array(s.length);
-    for (let i = 0; i < dp.length; i++) {
-        dp[ i ] = new Array(s.length);
-    }
+    function reverse(i, j) {
+        while (i < j) {
+            let tmp = A[ i ];
+            A[ i ] = A[ j ];
+            A[ j ] = tmp;
 
-    let start = 0, maxLen = 1;
-
-    for (let j = 1; j < s.length; j++) {
-        for (let i = 0; i < j; i++) {
-            if (s[ i ] !== s[ j ]) {
-                dp[ i ][ j ] = false;
-            } else {
-                if (j - i < 3) {
-                    dp[ i ][ j ] = true;
-                } else {
-                    dp[ i ][ j ] = dp[ i + 1 ][ j - 1 ];
-                }
-            }
-
-            if (dp[ i ][ j ] && j - i + 1 > maxLen) {
-                maxLen = j - i + 1;
-                start = i;
-            }
+            i++;
+            j--;
         }
     }
 
-    return s.substr(start, maxLen);
+    flip(A.length - 1);
+
+    return res;
 };
 
-console.log(test("abcda"))
+
+console.log(test([ 3,2,4,1 ]))
